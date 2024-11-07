caff498266152b48ca5fefc9ee22aa6e8179bdfb
ramakrishna0611 <120008716+ramakrishna0611@users.noreply.github.com>
2024-11-07 19:27:08 +0530

    Update json2html.js

--- json2html.js
+++ json2html.js
@@ -1,20 +1,15 @@
 // json2html.js
 export default function json2html(data) {
   const headers = ["Name", "Age", "Gender"];
+  
   const table = `
     <table data-user="abbidiramakrishnareddy@gmail.com">
       <thead>
-        <tr>${headers.map(header => <th>${header}</th>).join('')}</tr>
+        <tr>${headers.map(header => `<th>${header}</th>`).join('')}</tr>
       </thead>
       <tbody>
         ${data.map(row => `
           <tr>
-            ${headers.map(header => <td>${row[header] || ""}</td>).join('')}
+            ${headers.map(header => `<td>${row[header] || ""}</td>`).join('')}
           </tr>
-        `).join('')}
-      </tbody>
-    </table>
-  `;
-  
-  return table;
-  }
+       
